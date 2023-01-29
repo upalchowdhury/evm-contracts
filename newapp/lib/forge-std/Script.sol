921a4be3ad5c0df3e4d8c2b56996f6ff50c31546
upalchowdhury <upal.chowdhury@outlook.com>
2023-01-29 09:22:40 -0500

    update

--- newapp/lib/forge-std/Script.sol
+++ newapp/lib/forge-std/Script.sol
@@ -8,7 +8,7 @@ import "./console2.sol";
 abstract contract Script {
     bool public IS_SCRIPT = true;
     address constant private VM_ADDRESS =
-        address(bytes20(uint160(uint256(keccak256('hevm cheat code')))));
+        address(bytes20(uint160(uint256(keccak256('new address')))));
 
     Vm public constant vm = Vm(VM_ADDRESS);
 
